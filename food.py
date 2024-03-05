2a3a54bd89261ffd09320bba2a42e09192c4e0da
Daniel <danizmd8@gmail.com>
2024-03-05 19:13:52 +0300

    docs(food): Added docstrings to food

--- food.py
+++ food.py
@@ -12,15 +12,25 @@ _FOOD_SPAWN_PROBABILITIES = {
 
 
 class FoodFactory:
+    """
+    Creates a food on a scene
+    """
+
     def __init__(self, scene) -> None:
         self._scene = scene
 
         scene.add_factory(self)
 
     def update(self) -> None:
+        """
+        Spawns food in random localion at every tick
+        """
         self.spawn_radom_food()
 
     def spawn_radom_food(self) -> None:
+        """
+        Spawns food in radom location according to probabilities
+        """
         random_number = random()
 
         for level, food_spawn_probability in _FOOD_SPAWN_PROBABILITIES.items():
@@ -29,15 +39,31 @@ class FoodFactory:
                 return
 
     def spawn_bunch(self, amount: int = 1) -> None:
+        """
+        Spawns several food units
+
+        Args:
+            amount (int, optional): Amount of food to spawn. Defaults to 1.
+        """
         for _ in range(amount):
             self.spawn_radom_food()
 
     def _spawn_food(self, level: int) -> None:
+        """
+        Spawns food in radom localtion of scene with a given level
+
+        Args:
+            level (int): Level of food to be spawned
+        """
         coords = self._scene.get_random_empty_cell()
         self._scene.add_game_object(Food(coords, level, self._scene), coords)
 
 
 class Food(GameObject):
+    """
+    Represents a food
+    """
+
     def __init__(self, coords: tuple[int, int], level: int, scene) -> None:
         super().__init__(coords)
 
@@ -49,9 +75,21 @@ class Food(GameObject):
         return self._level
 
     def get_max_capacity(self) -> float:
+        """
+        Get food max capacity
+
+        Returns:
+            float: food capacity
+        """
         return 5 * self._level
 
     def get_exhaustion(self) -> float:
+        """
+        Get food every tick exhaustion
+
+        Returns:
+            float: exhaustion
+        """
         return 0.1 * self._level
 
     def update(self) -> None:
